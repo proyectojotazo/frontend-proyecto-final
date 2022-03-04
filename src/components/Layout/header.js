40fbdbc9bb07cb0b882036a00e8bdd33a7c03873
Elisabet D'Acosta <venganzaalchocolate@gmail.com>
2022-03-04 23:54:43 +0100

    header imagen y texto sin responsive

--- src/components/Layout/header.js
+++ src/components/Layout/header.js
@@ -1,13 +1,19 @@
 import '../../sass/contenedor.scss';
 import '../../sass/header.scss';
 import React from "react";
+import { ReactComponent as Gente } from '../../assets/gente.svg';
 
 class Header extends React.Component {
     render() {
         return(
             <header className="header">
-                <div className='imagen'></div>
-                <div className='texto'></div>
+                <div className='imagen'>
+                    <Gente/>
+                </div>
+                <div className='texto'>
+                    <h1>Crea, <br/> Escribe <br/> & Comparte</h1>
+                    <p>El último me voy.... nace de la necesidad de unos programadores de devolver a internet un poco de lo han cogido.... crea, escribe y comparte, únete a la comunidad. </p>
+                </div>
             </header>
         );
     }
