ea2e9fd6d752a534c03a2b40ee1d33e3cf297a91
MiwelR <rrriosss@gmail.com>
2022-03-08 00:18:43 +0100

    vamos alla

--- src/components/common/Articulos.js
+++ src/components/common/Articulos.js
@@ -1,36 +1,38 @@
 import React from "react";
-import { useEffect, useState } from 'react'
-import { getArticulos, getArticulosCategorias } from "../../api/services/articulos";
+import { useEffect, useState } from "react";
+import {
+  getArticulos,
+  getArticulosCategorias,
+} from "../../api/services/articulos";
 import Card from "./Card";
 import { Link } from "react-router-dom";
-import "../common/articulos.scss"
+import "../common/articulos.scss";
 
 function Articulos(props) {
-    const [articulos, setArticulos] = useState([])
-    const {categoria} = props;
+  const [articulos, setArticulos] = useState([]);
+  const { categoria } = props;
 
-    
-    useEffect(() => {
-        if (categoria)  {
-                getArticulosCategorias(categoria).then(x=>{setArticulos(x)})}
-            else {
-                getArticulos().then(x=>{setArticulos(x)})
-            }
-            
-    }, [categoria])
+  useEffect(() => {
+    if (categoria) {
+      getArticulosCategorias(categoria).then((x) => {
+        setArticulos(x);
+      });
+    } else {
+      getArticulos().then((x) => {
+        setArticulos(x);
+      });
+    }
+  }, [categoria]);
 
-    return (
-        <section className="seccionArticulos">
-            {articulos.map(({ id, ...advert }) => (
-                <Link to={`/articles/${id}`}>
-                    <Card {...advert} />
-                </Link>
-            ))}   
-    
-        </section>
-        )
-    
-
+  return (
+    <section className="seccionArticulos">
+      {articulos.map(({ id, ...advert }) => (
+        <Link key={`/articles/${id}`}>
+          <Card {...advert} />
+        </Link>
+      ))}
+    </section>
+  );
 }
 
-export default Articulos
+export default Articulos;
