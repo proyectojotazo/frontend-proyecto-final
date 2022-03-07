1f808a7d4d42a6870a3cce25f00f7080d70c570a
Josep Miquel <tommyshelby@disroot.org>
2022-03-07 23:27:27 +0100

    bug navbar fixed

--- src/components/Layout/nav.js
+++ src/components/Layout/nav.js
@@ -8,7 +8,6 @@ import { ReactComponent as Nuevo } from '../../assets/nuevo.svg';
 import { ReactComponent as Inicio } from '../../assets/inicio.svg';
 import { ReactComponent as Usuario } from '../../assets/usuario.svg';
 
-
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 
@@ -17,48 +16,41 @@ import LoginPopup from '../Auth/Login/Login';
 const MySwal = withReactContent(Swal);
 
 function Nav() {
-    return (
-        <nav className='nav'>
-            <div className='logo' >
-                <Link to='/'>
-                    <Logo width="150" height="100" />
-                </Link>
-            </div>
+  return (
+    <nav className="nav">
+      <div className="logo">
+        <Link to="/">
+          <Logo width="150" height="100" />
+        </Link>
+      </div>
 
-            <ul className="nav-list">
-                <li className='navbar-item'>
-                    <NavLink exact to='/' className='nav-link'>
-                        <Lupa
-                            className='icon icon-lupa'
-                        />
-                    </NavLink>
-                </li>
-                <li className='navbar-item'>
-                    <NavLink exact to='/' className='nav-link'>
-                        <Nuevo
-                            className='icon icon-nuevo'
-                        />
-                    </NavLink>
-                </li>
-                <li className='navbar-item'>
-                    <NavLink className='nav-link' exact to='/escribir'>
-                        <Inicio
-                            className='icon icon-inicio'
-                        />
-                    </NavLink>
-                </li>
-                <li className='navbar-item'>
-                    <NavLink className='nav-link' exact to='/ Iniciar Sesión'>
-                        <Usuario
-                            className='icon icon-usuario'
-                            onClick={() => LoginPopup()}
-                        />
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-    );
-
+      <ul className="nav-list">
+        <li className="navbar-item">
+          <NavLink exact to="/" className="nav-link">
+            <Lupa className="icon icon-lupa" />
+          </NavLink>
+        </li>
+        <li className="navbar-item">
+          <NavLink exact to="/" className="nav-link">
+            <Nuevo className="icon icon-nuevo" />
+          </NavLink>
+        </li>
+        <li className="navbar-item">
+          <NavLink className="nav-link" exact to="/escribir">
+            <Inicio className="icon icon-inicio" />
+          </NavLink>
+        </li>
+        <li className="navbar-item">
+          <div className="nav-link">
+            <Usuario
+              className="icon icon-usuario"
+              onClick={() => LoginPopup()}
+            />
+          </div>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
