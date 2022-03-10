976297646871d178380d8dd8d2145a4e9106a1b6
Elisabet D'Acosta <venganzaalchocolate@gmail.com>
2022-03-10 22:14:10 +0100

    terminando con las categorias de forma dinámica

--- src/components/common/BarraCategorias.js
+++ src/components/common/BarraCategorias.js
@@ -1,50 +1,23 @@
 import React from "react";
 import "./barraCategorias.scss"
-
+import {categorias} from "../../utils/categorias"
 
 function BarraCategorias ({cambiarCategoria}) {
-    const categorias= [
-        {color:"rojo", categorias:["todas"]},
-        {color:"cyan", categorias:["html", "css", "angular"]},
-        {color:"pink", categorias:["angular", "vue", "react"]},
-        {color:"orange", categorias:["php", "python", "java"]},
-        {color:"purple", categorias:["node", "laravel"]},
-        {color:"green", categorias:["mysql", "mongodb"]},
-    ];
-    const colores = [
-        {color: 'rojo', categoria:["html", "css", "angular"]},
-        {color: 'verde', categoria:["html", "css", "angular"]}
-    ]
+    
 
     return(
-        <section className="categorias">
-            <hr/>
-            <ul className="ulCategorias">
-                <li className="rojo" onClick={()=>{cambiarCategoria('')}}>TODAS</li>
-                <li className="cyan" onClick={()=>{cambiarCategoria('html')}}>HTML</li>
-                <li className="cyan" onClick={()=>{cambiarCategoria('css')}}>CSS</li>
-                <li className="cyan" onClick={()=>{cambiarCategoria('javascript')}}>JAVASCRIPT</li>
-                <li className="pink" onClick={()=>{cambiarCategoria('angular')}}>ANGULAR</li>
-                <li className="pink" onClick={()=>{cambiarCategoria('vue')}}>VUE</li>
-                <li className="pink" onClick={()=>{cambiarCategoria('react')}}>REACT</li>
-                <li className="orange" onClick={()=>{cambiarCategoria('php')}}>PHP</li>
-                <li className="orange" onClick={()=>{cambiarCategoria('python')}}>PYTHON</li>
-                <li className="orange" onClick={()=>{cambiarCategoria('java')}}>JAVA</li>
-                <li className="purple" onClick={()=>{cambiarCategoria('node')}}>NODE</li>
-                <li className="purple" onClick={()=>{cambiarCategoria('laravel')}}>LARAVEL</li>
-                <li className="green" onClick={()=>{cambiarCategoria('mysql')}}>MYSQL</li>
-                <li className="green" onClick={()=>{cambiarCategoria('mongodb')}}>MONGODB</li>
-            </ul>
-            <ul className="ulCategorias">
+            <section className="categorias">
+                <ul className="ulCategorias">
                 {categorias.map((elemento)=>(
-                    <div>
-                        <h1>{elemento.color}</h1>
-                        {elemento.categorias.map((x)=>(<h2>{x}</h2>))}
-                    </div>
+                        <div key={elemento.color}>
+                        {elemento.categorias.map((lenguaje)=>(
+                            <li key={lenguaje} className={elemento.color} onClick={()=>{cambiarCategoria((lenguaje==='todas') ?'':lenguaje)}}>{lenguaje.toLocaleUpperCase()}</li>
+                            ))}
+                        </div>
+                    
                     ))}
-            </ul>
-            <hr/>
-        </section>
+                </ul>
+            </section>
     );
 }
 
---
+++ src/utils/categorias.js
@@ -0,0 +1,8 @@
+export const categorias= [
+    {color:"rojo", categorias:["todas"]},
+    {color:"cyan", categorias:["html", "css", "javascript"]},
+    {color:"pink", categorias:["angular", "vue", "react"]},
+    {color:"orange", categorias:["php", "python", "java"]},
+    {color:"purple", categorias:["node", "laravel"]},
+    {color:"green", categorias:["mysql", "mongodb"]},
+];
