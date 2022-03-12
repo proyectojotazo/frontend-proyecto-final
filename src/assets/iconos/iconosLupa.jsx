e1d11c0a452728de210366d9f6a3a5ef960bd979
César Pinto <63161873+csrap@users.noreply.github.com>
2022-03-12 22:17:57 +0100

    navbar-footer-v2

---
+++ src/assets/iconos/iconosLupa.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function IconosLupa() {
+  return (
+    <div>
+      <svg id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 221.255 199.958"><defs><style>.cls-1{fill:#a992c4;}</style></defs><title>ICONOSin título-2LUPA</title><path className="cls-1" d="M170.742,140.819a91.658,91.658,0,1,0-14.768,18.28l51.474,42.173L222.343,183.1ZM92.677,159.9a67,67,0,1,1,67-67A67.074,67.074,0,0,1,92.677,159.9Z" transform="translate(-1.088 -1.314)" /></svg>
+    </div>
+
+  )
+}
--- src/components/Layout/Nav.js
+++ src/components/Layout/Nav.js
@@ -1,15 +1,17 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import "./Nav.scss";
 import "./Layout.scss";
+import "./NavBar/MenuBurger.scss";
 import { NavLink, Link } from "react-router-dom";
-import { ReactComponent as Logo } from "../../assets/LOGO.svg";
 import { ReactComponent as Lupa } from "../../assets/iconoLupa.svg";
 import { ReactComponent as Nuevo } from "../../assets/nuevo.svg";
 import { ReactComponent as Inicio } from "../../assets/inicio.svg";
 import { ReactComponent as Usuario } from "../../assets/usuario.svg";
-
+import LogoMa from '../../assets/logomadeja.png';
 import Login from "../Auth/Login/Login";
 import Popup from "../Auth/Popup/PopUp";
+import * as Icons from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 import { useAuth } from "../../contexts/authContext";
 
@@ -18,6 +20,8 @@ import SweetAlert2 from "react-sweetalert2";
 function Nav() {
   const [showLogin, setShowLogin] = useState({});
   const [showRegister, setShowRegister] = useState({});
+  const [mobile, setMobile] = useState(false);
+  const [sidebar, setSidebar] = useState(false);
 
   function LoginPopup() {
     setShowLogin({
@@ -27,50 +31,135 @@ function Nav() {
     });
   }
 
+  useEffect(() => {
+    if (window.innerWidth < 900) {
+      setMobile(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 900) {
+        setMobile(true);
+      } else {
+        setMobile(false);
+        setSidebar(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+
   return (
-    <nav className="nav">
-      <div className="logo">
-        <Link to="/">
-          <Logo width="150" height="100" />
-        </Link>
+    <>
+      <nav className="nav">
+        <div className="logo">
+          <Link to="/">
+            <img src={LogoMa} width="200" height="75" />
+          </Link>
+        </div>
+
+        {!mobile && (
+          < ul className="nav-list">
+            <li className="navbar-item">
+              <NavLink to="/" className="nav-link">
+                <Lupa className="icon icon-lupa" />
+              </NavLink>
+            </li>
+            <li className="navbar-item">
+              <NavLink to="/" className="nav-link">
+                <Nuevo className="icon icon-nuevo" />
+              </NavLink>
+            </li>
+            <li className="navbar-item">
+              <NavLink className="nav-link" to="/escribir">
+                <Inicio className="icon icon-inicio" />
+              </NavLink>
+            </li>
+            <li className="navbar-item">
+              <div className="nav-link">
+                <Usuario
+                  className="icon icon-usuario"
+                  onClick={() => LoginPopup()}
+                />
+                <SweetAlert2
+                  {...showLogin}
+                  didClose={() => {
+                    setShowLogin({
+                      show: false,
+                    });
+                  }}
+                >
+                  <Popup />
+                </SweetAlert2>
+              </div>
+            </li>
+          </ul>
+        )} {mobile}
+
+        {mobile && (
+          <div className="sidebar-toggle">
+            {sidebar ? (
+              <Icons.FaTimes
+                className="sidebar-toggle-logo"
+                onClick={() => setSidebar(!sidebar)}
+              />
+            ) : (
+              <Icons.FaBars
+                className="sidebar-toggle-logo"
+                onClick={() => setSidebar(!sidebar)}
+              />
+            )}
+          </div>
+        )}
+      </nav>
+
+      <div className={sidebar ? "sidebar active" : "sidebar"}>
+        <ul className="nav-list">
+          <li className="sidebar-item ">
+            <NavLink to="/" className="nav-link">
+              <Lupa className="icon icon-lupa" />
+            </NavLink>
+          </li>
+          <li className="sidebar-item ">
+            <NavLink to="/" className="nav-link">
+              <Nuevo className="icon icon-nuevo" />
+            </NavLink>
+          </li>
+          <li className="sidebar-item">
+            <NavLink className="nav-link" to="/escribir">
+              <Inicio className="icon icon-inicio" />
+            </NavLink>
+          </li>
+          <li className="sidebar-item">
+            <div className="nav-link">
+              <Usuario
+                className="icon icon-usuario"
+                onClick={() => LoginPopup()}
+              />
+              <SweetAlert2
+                {...showLogin}
+                didClose={() => {
+                  setShowLogin({
+                    show: false,
+                  });
+                }}
+              >
+                <Popup />
+              </SweetAlert2>
+            </div>
+          </li>
+        </ul>
+
+
       </div>
 
-      <ul className="nav-list">
-        <li className="navbar-item">
-          <NavLink to="/" className="nav-link">
-            <Lupa className="icon icon-lupa" />
-          </NavLink>
-        </li>
-        <li className="navbar-item">
-          <NavLink to="/" className="nav-link">
-            <Nuevo className="icon icon-nuevo" />
-          </NavLink>
-        </li>
-        <li className="navbar-item">
-          <NavLink className="nav-link" to="/escribir">
-            <Inicio className="icon icon-inicio" />
-          </NavLink>
-        </li>
-        <li className="navbar-item">
-          <div className="nav-link">
-            <Usuario
-              className="icon icon-usuario"
-              onClick={() => LoginPopup()}
-            />
-            <SweetAlert2
-              {...showLogin}
-              didClose={() => {
-                setShowLogin({
-                  show: false,
-                });
-              }}
-            >
-              <Popup />
-            </SweetAlert2>
-          </div>
-        </li>
-      </ul>
-    </nav>
+
+    </>
   );
 }
 
