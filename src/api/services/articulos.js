8f14b02039a7d0829ee36af955c0483ec89e5658
MiwelR <rrriosss@gmail.com>
2022-03-12 19:58:36 +0100

    Paginación v2

--- src/api/services/articulos.js
+++ src/api/services/articulos.js
@@ -14,7 +14,7 @@ export const getArticulosId = async (id = "") => {
 
 export const getArticulosCategorias = async (categoria, orden, pagina) => {
   const categoriaFilter = categoria && `&categorias=${categoria}`;
-  const url = `${process.env.REACT_APP_API_BASE_URL}/articles?sort=${orden}&skip=${pagina}&limit=3${categoriaFilter}`;
+  const url = `${process.env.REACT_APP_API_BASE_URL}/articles?sort=${orden}&skip=${pagina}&limit=6${categoriaFilter}`;
   try {
     return await client.get(url);
   } catch (error) {
