7e5d9cb1645579d72e387cafb8cac1b16df8a483
Elisabet D'Acosta <venganzaalchocolate@gmail.com>
2022-03-12 18:17:49 +0100

    modificaciones en card

--- src/components/common/Info.js
+++ src/components/common/Info.js
@@ -1,33 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext } from "react";
 import "./info.scss"
-import { FaRegHeart, FaRegComments, FaRegPaperPlane, FaRegEdit } from "react-icons/fa"
+import { FaHeart, FaRegHeart, FaRegComments, FaRegPaperPlane, FaRegEdit } from "react-icons/fa"
+import AuthContext from "../../contexts/authContext";
 
-function Info ({...articulo}) {
-    const date = new Date(articulo.fechaPublicacion)
+function Info ({art}) {
+    const date = new Date(art.fechaPublicacion)
     const representarFecha = date.toDateString()
+    const { isLogged } = useContext(AuthContext)
 
-    const [seguir, setSeguir] = useState('')
-
-    useEffect(() => {
-        console.log('hola')
-    }, [seguir]);
-    
 
     return (
         <div className="info padre">
                 <div className="info">
                     <div className="avatar"><img src={`${process.env.REACT_APP_API_BASE_URL}/upload/avatar_default.jpg`}></img></div>
                     <div>
-                        <p className="usuario">{articulo.usuario[0].nickname}</p>
+                        <p className="usuario">{art.usuario[0].nickname}</p>
                         <p className="fecha">{representarFecha}</p>
                     </div>
                 </div>
                 <div className="iconosInfo">
                     <ul className="uinfo">
-                        <li className="listaIconos "><FaRegComments className="iconos comentarios"/>5 </li>
-                        <li className="listaIconos "><FaRegHeart className="iconos corazon"/>4</li>
-                        <li className="listaIconos "><FaRegPaperPlane className="iconos enviar"/></li>
+                        <li className="listaIconos "><FaRegComments className="iconos comentarios"/>{art.comentarios.length}</li>
+                        {isLogged && 
+                        <>
+                        <li className="listaIconos "><FaRegHeart className="iconos corazon"/></li>
                         <li className="listaIconos "><FaRegEdit className="iconos contestar"/></li>
+                        </>}
+                        <li className="listaIconos "><FaRegPaperPlane className="iconos enviar"/></li>
+                        
                     </ul>
                 </div>
         </div>
