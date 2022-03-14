6805690067cfa879460b14a74355ccffe3fa4f93
tommyshelby1917 <tommyshelby@disroot.org>
2022-03-14 21:36:15 +0100

    Css hover added

--- src/pages/RecoverAccount.js
+++ src/pages/RecoverAccount.js
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
-import { useParams, Navigate } from "react-router-dom";
+import React, { useState } from 'react'
+import { useParams, Navigate } from 'react-router-dom'
 
-import { useForm } from "react-hook-form";
-import { ErrorMessage } from "@hookform/error-message";
+import { useForm } from 'react-hook-form'
+import { ErrorMessage } from '@hookform/error-message'
 
-import { ReactComponent as Usuario } from "../assets/usuario.svg";
+import { ReactComponent as Usuario } from '../assets/usuario.svg'
 
-import { recoverAccount } from "../api/services/auth";
+import { recoverAccount } from '../api/services/auth'
 
 const RecoverAccount = () => {
-  const { id, token } = useParams();
+  const { id, token } = useParams()
 
-  const [newPassword, setNewPassword] = useState({});
-  const [submittedSucces, setSubmittedSucces] = useState(true);
+  const [newPassword, setNewPassword] = useState({})
+  const [submittedSucces, setSubmittedSucces] = useState(false)
 
   const {
     register,
@@ -20,35 +20,35 @@ const RecoverAccount = () => {
     setError,
     clearErrors,
     formState: { errors },
-  } = useForm();
+  } = useForm()
 
   const handleInputChange = (event) => {
-    const { name, value } = event.target;
+    const { name, value } = event.target
     setNewPassword({
       ...newPassword,
       [name]: value,
-    });
-    clearErrors(name);
-  };
+    })
+    clearErrors(name)
+  }
 
   const handleRecoverAccount = () => {
-    const data = { id, token, password: newPassword.password };
+    const data = { id, token, password: newPassword.password }
     recoverAccount(data)
       .then(() => {
         setTimeout(() => {
-          setSubmittedSucces(true);
-        }, 3000);
+          setSubmittedSucces(true)
+        }, 3000)
       })
       .catch((err) => {
-        setError("custom", {
-          type: "manual",
+        setError('custom', {
+          type: 'manual',
           message: err.message,
-        });
-      });
-  };
+        })
+      })
+  }
 
   if (submittedSucces) {
-    return <Navigate to="/" replace={true} />;
+    return <Navigate to="/" replace={true} />
   }
 
   return (
@@ -61,13 +61,13 @@ const RecoverAccount = () => {
         <form noValidate onSubmit={handleSubmit(handleRecoverAccount)}>
           <div className="input-container">
             <input
-              {...register("password", {
-                required: "Introduce la nueva contraseña",
+              {...register('password', {
+                required: 'Introduce la nueva contraseña',
                 pattern: {
                   value:
                     /^(?=.*[a-zÀ-ÿ\u00f1\u00d1])(?=.*[A-ZÀ-ÿ\u00f1\u00d1])(?=.*\d)(?=.*[@$!%*?&\-_])[A-Za-zÀ-ÿ\u00f1\u00d1\d@$!%*?&\-_]{8,}$/,
                   message:
-                    "Mínimo 8 carácteres con 1 letra mayúscula, 1 minúscula, 1 número y 1 carácter especial de los siguientes: @$!%*?&-_",
+                    'Mínimo 8 carácteres con 1 letra mayúscula, 1 minúscula, 1 número y 1 carácter especial de los siguientes: @$!%*?&-_',
                 },
               })}
               type="password"
@@ -87,8 +87,8 @@ const RecoverAccount = () => {
 
           <div className="input-container">
             <input
-              {...register("repeatPassword", {
-                required: "Introduce de nuevo la contraseña",
+              {...register('repeatPassword', {
+                required: 'Introduce de nuevo la contraseña',
                 validate: {
                   repeatPwd: (value) => value === newPassword.password,
                 },
@@ -100,7 +100,7 @@ const RecoverAccount = () => {
               placeholder="Repite la contraseña"
             />
             {errors.repeatPassword &&
-              errors.repeatPassword.type === "repeatPwd" && (
+              errors.repeatPassword.type === 'repeatPwd' && (
                 <p className="form-custom-error">
                   Tus contraseñas no coinciden
                 </p>
@@ -131,7 +131,7 @@ const RecoverAccount = () => {
         </form>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default RecoverAccount;
+export default RecoverAccount
