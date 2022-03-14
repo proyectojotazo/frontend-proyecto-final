ba2ed45a938041316a9cb454365024b2eab4cc54
César Pinto <63161873+csrap@users.noreply.github.com>
2022-03-14 01:43:58 +0100

    delete navbar responsive with useEffect

--- src/components/Layout/Nav.js
+++ src/components/Layout/Nav.js
@@ -18,9 +18,7 @@ import { useAuth } from "../../contexts/authContext";
 import SweetAlert2 from "react-sweetalert2";
 
 function Nav() {
-  const [showLogin, setShowLogin] = useState({});
-  const [mobile, setMobile] = useState(false);
-  const [sidebar, setSidebar] = useState(false);
+  const [showLogin, setShowLogin] = useState([]);
 
   function LoginPopup() {
     setShowLogin({
@@ -30,131 +28,51 @@ function Nav() {
     });
   }
 
-  useEffect(() => {
-    if (window.innerWidth < 900) {
-      setMobile(true);
-    }
-  }, []);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth < 900) {
-        setMobile(true);
-      } else {
-        setMobile(false);
-        setSidebar(false);
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
-    <>
-      <nav className="nav">
-        <div className="logo">
-          <Link to="/">
-            <Logo className="logoImg" />
-            {/* <img src={LogoMa} className="logoImg"/> */}
-          </Link>
-        </div>
-        {!mobile && (
-          <ul className="nav-list">
-            <li className="navbar-item">
-              <NavLink to="/" className="nav-link">
-                <Lupa className="icon icon-lupa" />
-              </NavLink>
-            </li>
-            <li className="navbar-item">
-              <NavLink to="/" className="nav-link">
-                <Nuevo className="icon icon-nuevo" />
-              </NavLink>
-            </li>
-            <li className="navbar-item">
-              <NavLink className="nav-link" to="/escribir">
-                <Inicio className="icon icon-inicio" />
-              </NavLink>
-            </li>
-            <li className="navbar-item">
-              <div className="nav-link">
-                <Usuario
-                  className="icon icon-usuario"
-                  onClick={() => LoginPopup()}
-                />
-                <SweetAlert2
-                  {...showLogin}
-                  didClose={() => {
-                    setShowLogin({
-                      show: false,
-                    });
-                  }}
-                >
-                  <Popup />
-                </SweetAlert2>
-              </div>
-            </li>
-          </ul>
-        )}{" "}
-        {mobile}
-        {mobile && (
-          <div className="sidebar-toggle">
-            {sidebar ? (
-              <Icons.FaTimes
-                className="sidebar-toggle-logo"
-                onClick={() => setSidebar(!sidebar)}
-              />
-            ) : (
-              <Icons.FaBars
-                className="sidebar-toggle-logo"
-                onClick={() => setSidebar(!sidebar)}
-              />
-            )}
-          </div>
-        )}
-      </nav>
-
-      <div className={sidebar ? "sidebar active" : "sidebar"}>
-        <ul className="nav-list">
-          <li className="sidebar-item ">
-            <NavLink to="/" className="nav-link">
-              <Lupa className="icon icon-lupa" />
-            </NavLink>
-          </li>
-          <li className="sidebar-item ">
-            <NavLink to="/" className="nav-link">
-              <Nuevo className="icon icon-nuevo" />
-            </NavLink>
-          </li>
-          <li className="sidebar-item">
-            <NavLink className="nav-link" to="/escribir">
-              <Inicio className="icon icon-inicio" />
-            </NavLink>
-          </li>
-          <li className="sidebar-item">
-            <div className="nav-link">
-              <Usuario
-                className="icon icon-usuario"
-                onClick={() => LoginPopup()}
-              />
-              <SweetAlert2
-                {...showLogin}
-                didClose={() => {
-                  setShowLogin({
-                    show: false,
-                  });
-                }}
-              >
-                <Popup />
-              </SweetAlert2>
-            </div>
-          </li>
-        </ul>
+    <nav className="nav">
+      <div className="logo">
+        <Link to="/">
+          <Logo className="logoImg" />
+        </Link>
       </div>
-    </>
-  );
-}
 
+      <ul className="nav-list">
+        <li className="navbar-item">
+          <NavLink to="/" className="nav-link">
+            <Lupa className="icon icon-lupa" />
+          </NavLink>
+        </li>
+        <li className="navbar-item">
+          <NavLink to="/" className="nav-link">
+            <Nuevo className="icon icon-nuevo" />
+          </NavLink>
+        </li>
+        <li className="navbar-item">
+          <NavLink className="nav-link" to="/escribir">
+            <Inicio className="icon icon-inicio" />
+          </NavLink>
+        </li>
+        <li className="navbar-item">
+          <div >
+            <Usuario
+              className="icon icon-usuario"
+              onClick={() => LoginPopup()}
+            />
+            <SweetAlert2
+              {...showLogin}
+              didClose={() => {
+                setShowLogin({
+                  show: false,
+                });
+              }}
+            >
+              <Popup />
+            </SweetAlert2>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  );
+
+}
 export default Nav;
