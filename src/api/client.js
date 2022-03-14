4819802a60d89ff8cab41428a777c8eb5a365742
Javier <javigh87@gmail.com>
2022-03-14 13:15:45 +0100

    Arreglo interceptors client

--- src/api/client.js
+++ src/api/client.js
@@ -1,29 +1,32 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const client = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
-});
+})
 
 client.interceptors.response.use(
   (response) => response.data,
   (error) => {
+    if (error.response.status === 302) {
+      return Promise.resolve(error.response.data)
+    }
     if (!error.response) {
-      return Promise.reject({ message: error.message });
+      return Promise.reject({ message: error.message })
     }
     return Promise.reject({
       message: error.response.statusText,
       ...error.response,
       ...error.response.data,
-    });
+    })
   }
-);
+)
 
 export const setAuthorizationHeader = (token) => {
-  client.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-};
+  client.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
 
 export const removeAuthorizationHeader = () => {
-  delete client.defaults.headers.common['Authorization'];
-};
+  delete client.defaults.headers.common['Authorization']
+}
 
-export default client;
+export default client
