2287ff732ad8336a32e01f985220971e32be8038
Venganzaalchocolate <venganzaalchocolate@gmail.com>
2022-03-14 21:07:58 +0100

    ultimos cambios

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,16 +1,7 @@
 import Articulo from '../components/common/Articulo'
 
-<<<<<<< HEAD
-function Article () {
-    return (
-        <>
-            <Articulo/>
-        </>
-    )
-=======
 function Article() {
   return <Articulo />
->>>>>>> 7e7b944d526e9389a8d9f248f0527e6bae803393
 }
 
 export default Article
