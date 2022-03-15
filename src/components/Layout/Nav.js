ee73ae0e8eb7bc5f2bd148dec28667d4cc242316
MiwelR <rrriosss@gmail.com>
2022-03-15 12:43:56 +0100

    Dropdown Menu User

--- src/components/Layout/Nav.js
+++ src/components/Layout/Nav.js
@@ -1,31 +1,37 @@
-import React, { useContext, useState, useEffect } from "react";
-import "./Nav.scss";
-import "./Layout.scss";
-import "./NavBar/MenuBurger.scss";
-import { NavLink, Link } from "react-router-dom";
-import { ReactComponent as Logo } from "../../assets/logomadeja.svg";
-import { ReactComponent as Lupa } from "../../assets/iconoLupa.svg";
-import { ReactComponent as Nuevo } from "../../assets/nuevo.svg";
-import { ReactComponent as Inicio } from "../../assets/inicio.svg";
-import { ReactComponent as Usuario } from "../../assets/usuario.svg";
-import Login from "../Auth/Login/Login";
-import Popup from "../Auth/Popup/PopUp";
-import * as Icons from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
+import React, { useContext, useState, useEffect } from 'react'
+import './Nav.scss'
+import './Layout.scss'
+import './NavBar/MenuBurger.scss'
+import { NavLink, Link } from 'react-router-dom'
+import { ReactComponent as Logo } from '../../assets/logomadeja.svg'
+import { ReactComponent as Lupa } from '../../assets/iconoLupa.svg'
+import { ReactComponent as Nuevo } from '../../assets/nuevo.svg'
+import { ReactComponent as Inicio } from '../../assets/inicio.svg'
+import { ReactComponent as Usuario } from '../../assets/usuario.svg'
+import Login from '../Auth/Login/Login'
+import Popup from '../Auth/Popup/PopUp'
+import * as Icons from 'react-icons/fa'
+import { FaFacebook } from 'react-icons/fa'
 
-import { useAuth } from "../../contexts/authContext";
+import { useAuth } from '../../contexts/authContext'
 
-import SweetAlert2 from "react-sweetalert2";
+import SweetAlert2 from 'react-sweetalert2'
 
 function Nav() {
-  const [showLogin, setShowLogin] = useState([]);
+  const [showLogin, setShowLogin] = useState([])
+  const [userMenu, setUserMenu] = useState(false)
+  const { isLogged, accountLogout } = useAuth()
 
   function LoginPopup() {
     setShowLogin({
       show: true,
       showConfirmButton: false,
       showCloseButton: true,
-    });
+    })
+  }
+
+  const showUserMenu = () => {
+    setUserMenu(!userMenu)
   }
 
   return (
@@ -53,26 +59,46 @@ function Nav() {
           </NavLink>
         </li>
         <li className="navbar-item">
-          <div >
-            <Usuario
-              className="icon icon-usuario"
-              onClick={() => LoginPopup()}
-            />
-            <SweetAlert2
-              {...showLogin}
-              didClose={() => {
-                setShowLogin({
-                  show: false,
-                });
-              }}
-            >
-              <Popup />
-            </SweetAlert2>
+          <div>
+            {!isLogged ? (
+              <>
+                <Usuario
+                  className="icon icon-usuario"
+                  onClick={() => LoginPopup()}
+                />
+                <SweetAlert2
+                  {...showLogin}
+                  didClose={() => {
+                    setShowLogin({
+                      show: false,
+                    })
+                  }}
+                >
+                  <Popup />
+                </SweetAlert2>
+              </>
+            ) : (
+              <>
+                <Usuario
+                  className="icon icon-usuario"
+                  onClick={() => showUserMenu()}
+                />
+                {userMenu && (
+                  <ul className="userMenu">
+                    <li>
+                      <NavLink to="/my-account" className="dropdown-user">
+                        Perfil
+                      </NavLink>
+                    </li>
+                    <li onClick={accountLogout}>Cerrar Sesión</li>
+                  </ul>
+                )}
+              </>
+            )}
           </div>
         </li>
       </ul>
     </nav>
-  );
-
+  )
 }
-export default Nav;
+export default Nav
