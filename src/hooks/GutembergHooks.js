ea9b6a514fa2a7ae2e63d4b8bf1e658e1c02f0fc
tommyshelby1917 <tommyshelby@disroot.org>
2022-03-16 02:07:55 +0100

    Serialize to html added and cache content

--- src/hooks/GutembergHooks.js
+++ src/hooks/GutembergHooks.js
@@ -1,3 +1,6 @@
+import escapeHtml from 'escape-html';
+import { Text, Node } from 'slate';
+
 export const CodeElement = (props) => {
     return (
         <pre {...props.attributes}>
@@ -19,4 +22,40 @@ export const Leaf = (props) => {
             {props.children}
         </span>
     );
+};
+
+export const serialize = (node) => {
+    if (Text.isText(node)) {
+        let string = escapeHtml(node.text);
+        if (node.bold) {
+            string = `<strong>${string}</strong>`;
+        }
+        return string;
+    }
+
+    const children = node.children.map((n) => serialize(n)).join('');
+
+    switch (node.type) {
+        case 'quote':
+            return `<blockquote><p>${children}</p></blockquote>`;
+        case 'paragraph':
+            return `<p>${children}</p>`;
+        case 'link':
+            return `<a href="${escapeHtml(node.url)}">${children}</a>`;
+        case 'code':
+            return `<pre><code>${children}</code></pre>`;
+        default:
+            return `<p>${children}</p>`;
+        // return children;
+    }
+};
+
+// Define a deserializing function that takes a string and returns a value.
+export const deserialize = (string) => {
+    // Return a value array of children derived by splitting the string.
+    return string.split('\n').map((line) => {
+        return {
+            children: [{ text: line }],
+        };
+    });
 };
