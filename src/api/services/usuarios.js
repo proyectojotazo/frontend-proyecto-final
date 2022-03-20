7acfdf3de7f4b96a0eb5c51f6eff7b92900e84f1
Javier <javigh87@gmail.com>
2022-03-20 10:56:18 +0100

    Creando funcionalidad de seguir en UserInfo

--- src/api/services/usuarios.js
+++ src/api/services/usuarios.js
@@ -4,3 +4,8 @@ export const getUserById = async (id = '') => {
   const url = `${process.env.REACT_APP_API_BASE_URL}/users/${id}`
   return await client.get(url)
 }
+
+export const followUser = async (id) => {
+  const url = `${process.env.REACT_APP_API_BASE_URL}/users/follow/${id}`
+  return await client.post(url)
+}
