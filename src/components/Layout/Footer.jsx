e27b3a23f5144b00faf78107b3cde7457826a032
César Pinto <63161873+csrap@users.noreply.github.com>
2022-03-30 21:58:47 +0200

    footer-style

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -5,10 +5,12 @@ import { ReactComponent as FooterCafe } from '../../assets/footerCafe.svg';
 import { useAuth } from '../../contexts/authContext';
 import Contact from '../common/Contact';
 
+import { FaRegCopyright } from "react-icons/fa";
+
 
 function Footer() {
-    
-    const {t} = useAuth();
+
+    const { t } = useAuth();
     return (
         <footer className="footer">
             <div className="footer__container_footer">
@@ -16,43 +18,43 @@ function Footer() {
                     <h1 className='tit'>{t('footer.title')}</h1>
                     <h3 className='sub'>{t('footer.subtitle')}</h3>
                     <div className='equipo'>
-                    <Contact
-                    imagen="cesar_pinto.jpg"
-                    nombre="Cesar"
-                    git="csrap"
-                    linkedin="cesar-augusto-pinto"
-                    mail="caugusto3110@gmail.com"
-                    />
-                    <Contact
-                        imagen="miguel.jpg"
-                        nombre="Miguel Ángel"
-                        git="MiwelR"
-                        linkedin="miguelangeldelosrios"
-                        mail="rrriosss@gmail.com"
-                    />
-                    <Contact
-                        imagen="josep.jpeg"
-                        nombre="Josep Miquel"
-                        git="tommyshelby1917"
-                        linkedin="josep-miquel-arenas-beltran"
-                        mail="josepmiquel@exilium.cat"
-                    />
-                    <Contact
-                        imagen="javier.jpg"
-                        nombre="Javier"
-                        git="Jotazo"
-                        linkedin="javier-guerrero-huete-7032101b9"
-                        mail="javigh87@gmail.com"
-                    />
-                    <Contact
-                        imagen="elisabet.jpg"
-                        nombre="Elisabet"
-                        git="Venganzaalchocolate"
-                        linkedin="elisabetdacostaalmiron"
-                        mail="venganzaalchocolate@gmail.com"
-                    />
+                        <Contact
+                            imagen="cesar_pinto.jpg"
+                            nombre="Cesar"
+                            git="csrap"
+                            linkedin="cesar-augusto-pinto"
+                            mail="caugusto3110@gmail.com"
+                        />
+                        <Contact
+                            imagen="miguel.jpg"
+                            nombre="Miguel Ángel"
+                            git="MiwelR"
+                            linkedin="miguelangeldelosrios"
+                            mail="rrriosss@gmail.com"
+                        />
+                        <Contact
+                            imagen="josep.jpeg"
+                            nombre="Josep Miquel"
+                            git="tommyshelby1917"
+                            linkedin="josep-miquel-arenas-beltran"
+                            mail="josepmiquel@exilium.cat"
+                        />
+                        <Contact
+                            imagen="javier.jpg"
+                            nombre="Javier"
+                            git="Jotazo"
+                            linkedin="javier-guerrero-huete-7032101b9"
+                            mail="javigh87@gmail.com"
+                        />
+                        <Contact
+                            imagen="elisabet.jpg"
+                            nombre="Elisabet"
+                            git="Venganzaalchocolate"
+                            linkedin="elisabetdacostaalmiron"
+                            mail="venganzaalchocolate@gmail.com"
+                        />
                     </div>
-                    
+
                 </div>
                 <div className="footer__footerImagen">
                     <FooterCafe />
@@ -62,6 +64,13 @@ function Footer() {
                     <p>
                         {t("footer.copyright")}
                     </p>
+                    <p>
+                        <FaRegCopyright
+                            className="copyright-icon"
+                        />
+                        <span className="text-copy"> 2022 Copyright </span>
+                    </p>
+
                     <p>El Ultimo & Me Voy</p>
                 </div>
             </div>
